91e0e563a825e194ba4694962bfdedd894706fe1
itisameerkhan <itisameerkhan@gmail.com>
2024-07-06 14:34:04 +0530

    update

--- src/Pages/Movies/Movies.jsx
+++ src/Pages/Movies/Movies.jsx
@@ -34,6 +34,16 @@ const Movies = () => {
     isAvailable: true,
     releaseDate: 0,
   });
+
+  const [updateData, setUpdateData] = useState({
+    title: "",
+    description: "",
+    poster_img: "",
+    IMDB: 0,
+    isAvailable: true,
+    releaseDate: 0,
+  });
+
   const [snackStatus, setSnackStatus] = useState({
     open: false,
     message: "",
@@ -86,9 +96,14 @@ const Movies = () => {
       console.log(e);
     }
   };
+
   const handleChange = (e) => {
     setMovieData({
       ...movieData,
+      [e.target.name]: e.target.value,
+    });
+    setUpdateData({
+      ...updateData,
       [e.target.name]: e.target.value,
     });
   };
@@ -106,6 +121,8 @@ const Movies = () => {
       console.log(e);
     }
   };
+
+  const updateMovie = async () => {};
 
   useEffect(() => {
     getMovieData();
@@ -199,42 +216,104 @@ const Movies = () => {
       </div>
       <div className="movies-main">
         {movieList.map((data) => (
-          <Card sx={{ maxWidth: 345 }} key={data.id}>
-            <CardMedia
-              component="img"
-              alt="green iguana"
-              height="350"
-              image={data.poster_img}
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                {data.title}
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                {data.description}
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                imdb: {data.IMDB} - Release Date: {data.releaseDate}
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button
-                size="small"
-                onClick={() => setDialogStatus({ ...dialogStatus, open: true })}
+          <div key={data.id}>
+            <Card sx={{ maxWidth: 345 }} key={data.id}>
+              <CardMedia
+                component="img"
+                alt="green iguana"
+                height="350"
+                image={data.poster_img}
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {data.title}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  {data.description}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  imdb: {data.IMDB} - Release Date: {data.releaseDate}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button
+                  size="small"
+                  onClick={() =>
+                    setDialogStatus({ ...dialogStatus, open: true })
+                  }
+                >
+                  UPDATE
+                </Button>
+                <Button
+                  size="small"
+                  color="error"
+                  onClick={() => {
+                    deleteMovie(data.id);
+                  }}
+                >
+                  Delete
+                </Button>
+              </CardActions>
+            </Card>
+            <Dialog open={dialogStatus.open} onClose={handleDialogClose}>
+              <div
+                style={{ padding: "1rem", width: "500px" }}
+                className="dialog-1"
               >
-                UPDATE
-              </Button>
-              <Button
-                size="small"
-                color="error"
-                onClick={() => {
-                  deleteMovie(data.id);
-                }}
-              >
-                Delete
-              </Button>
-            </CardActions>
-          </Card>
+                <TextField
+                  id="outlined-basic-4"
+                  label="Title"
+                  variant="outlined"
+                  name="title"
+                  type="text"
+                  required
+                  style={{ width: "100%" }}
+                  value={data.title}
+                />
+                <TextField
+                  id="outlined-basic-4"
+                  label="Description"
+                  variant="outlined"
+                  name="description"
+                  type="text"
+                  required
+                  style={{ width: "100%" }}
+                  value={data.description}
+                />
+                <TextField
+                  id="outlined-basic-4"
+                  label="Poster Image"
+                  variant="outlined"
+                  name="poster_img"
+                  type="text"
+                  required
+                  style={{ width: "100%" }}
+                  value={data.poster_img}
+                />
+                <TextField
+                  id="outlined-basic-4"
+                  label="IMDB"
+                  variant="outlined"
+                  name="IMDB"
+                  type="number"
+                  required
+                  style={{ width: "100%" }}
+                  value={data.IMDB}
+                />
+                <TextField
+                  id="outlined-basic-4"
+                  label="Release Date"
+                  variant="outlined"
+                  name="release_date"
+                  type="text"
+                  required
+                  style={{ width: "100%" }}
+                  value={data.releaseDate}
+                />
+                <Button variant="contained">SUBMIT</Button>
+              </div>
+            </Dialog>
+          </div>
         ))}
       </div>
       <Snackbar
@@ -243,56 +322,6 @@ const Movies = () => {
         onClose={handleClose}
         message={snackStatus.message}
       />
-      <Dialog open={dialogStatus.open} onClose={handleDialogClose}>
-        <div style={{ padding: "1rem", width: "500px" }} className="dialog-1">
-          <TextField
-            id="outlined-basic-4"
-            label="Title"
-            variant="outlined"
-            name="title"
-            type="text"
-            required
-            style={{ width: "100%" }}
-          />
-          <TextField
-            id="outlined-basic-4"
-            label="Description"
-            variant="outlined"
-            name="description"
-            type="text"
-            required
-            style={{ width: "100%" }}
-          />
-          <TextField
-            id="outlined-basic-4"
-            label="Poster Image"
-            variant="outlined"
-            name="poster_img"
-            type="text"
-            required
-            style={{ width: "100%" }}
-          />
-          <TextField
-            id="outlined-basic-4"
-            label="IMDB"
-            variant="outlined"
-            name="IMDB"
-            type="number"
-            required
-            style={{ width: "100%" }}
-          />
-          <TextField
-            id="outlined-basic-4"
-            label="Release Date"
-            variant="outlined"
-            name="release_date"
-            type="text"
-            required
-            style={{ width: "100%" }}
-          />
-          <Button variant="contained">SUBMIT</Button>
-        </div>
-      </Dialog>
     </div>
   );
 };
